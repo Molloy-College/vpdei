3f3856f3c4c74b71f686ea264247a63e795beefa
rontinez <senditronnn@gmail.com>
2021-11-02 16:34:21 -0400

    update opening line; remove current title and other candidates

--- bodycopy_resumes.php
+++ bodycopy_resumes.php
@@ -12,35 +12,7 @@
                     <div class="stdpage-v8-old hidebodycopy-notset body-copy-section">
                         <div class="row">
                             <p>
-                            On behalf of the VP DEI Search Committee, we are pleased to announce that after an extensive national search, the field of candidates for the Vice President for Diversity, Equity, and Inclusion has selected three finalists.  Each of the finalists will be coming to the Rockville Centre campus over the next two weeks to meet with various groups that comprise the Molloy College community.  In addition to some small group meetings, we welcome all college community members to hear from the candidates at two “open” sessions.  The schedule for the open sessions can be found below.  Please note that there are two options for you to attend an open session for each candidate.  </p>
-
-                            <br>
-                            <ul>
-                            <li>
-                                Candidate #1           Thursday, November 4th              10:30-11:15am                   Madison Theatre
-                            </li>
-                            <li>
-                                Candidate #1           Thursday, November 4th              4:00-4:45pm                       Madison Theatre
-                            </li>
-
-                            <br>
-                            
-                            <li>
-                            Candidate #2           Monday, November 8th                10:30-11:15am                   Madison Theatre
-                            </li>
-                            <li>
-                            Candidate #2           Monday, November 8th                 4:00-4:45pm                       Madison Theatre
-                            </li>
-                            
-                            <br>
-                            
-                            <li>
-                            Candidate #3           Tuesday, November 9th                10:30-11:15am                   Madison Theatre
-                            </li>
-                            <li>
-                            Candidate #3           Tuesday, November 9th                1:00-1:45pm                       Madison Theatre
-                            </li>
-                            <ul>
+                            On behalf of the VP DEI Search Committee, we are pleased to announce that after an extensive national search, the field of candidates for the Vice President for Diversity, Equity, and Inclusion has selected three finalists.  Each of the finalists will be coming to the Rockville Centre campus over the next two weeks to meet with various groups that comprise the Molloy College community. Below is the CV for Candidate #2:</p>
                             <br>
 
                             <p>
@@ -55,40 +27,11 @@
                             <!-- //////////////////////////////////////// -->
 
                             <!-- 
-                                Benita Wolff      post at 5pm am on November 3rd      Open meeting begins on the 4th at 10:30 am and 4 pm; please take down at 5 pm on November 4th
-
  
                                 Donald "DJ" Mitchell      post at 5pm on November 7th             Open meeting begins on the 8th at 10:30 am and 4 pm; please take down at 5 pm on November 8th
-
-                                
-                                Kahan Sablo        post at 5pm am on November 8th      Open meeting begins on the 9th at 10:30 am and 1 pm on the 9th; please take down at 5 pm on the 9th. 
-                            -->
-
-                            <!-- 
-                                PLACE BELOW "VIEW RESUME" <a> tag    
-                                <a style="float: right;" href="https://www.surveymonkey.com/r/MolloyProvostCandidateFeedbackSH" target="_blank">Submit Feedback</a> 
                             -->
 
                             <div class="row">
-
-							<!-- BW (POST AT 5pm am on November 3rd) -->
-                                <!-- <div class="col-sm-12" style="margin: 20px auto;background-color: #fff; border: 1px solid #ccc; border-radius: 2px/4px; box-shadow: 0 0 5px #ccc;height: 200px; padding: 12px 10px;position: relative;">
-                                    <div class="content">
-                                        <p style='font-weight: bold;'>Candidate #1: Thursday, November 4th, 2021 (10:30 - 11:15am) & (4:00 - 4:45pm)</p>
-                                        <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
-                                        <h4 style="font-weight: bold;">Benita Wolff</h4>
-                                        <p style="font-size: 12px;">Director of Diversity and Inclusion<br><br>
-                                        </p>
-
-                                        <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
-                                        <div class="description">
-                                            <a href="vpdei/documents/resumes/BW-CV.pdf" target="_blank">View Resume</a>
-                                            <a style="float: right;" href="https://www.surveymonkey.com/r/MolloyProvostCandidateFeedbackSH" target="_blank">Submit Feedback</a> 
-
-                                        </div>
-                                    </div>
-
-                                </div> -->
 
 							<!-- DJ (POST AT 5pm on November 7th) -->
                                 <div class="col-sm-12" style="margin: 20px auto;background-color: #fff; border: 1px solid #ccc; border-radius: 2px/4px; box-shadow: 0 0 5px #ccc;height: 200px; padding: 12px 10px;position: relative;">
@@ -96,36 +39,16 @@
                                         <p style='font-weight: bold;'>Candidate #2: Monday, November 8th, 2021 (10:30 - 11:15am) & (4:00 - 4:45pm)</p>
                                         <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
                                         <h4 style="font-weight: bold;">Donald "DJ" Mitchell</h4>
-                                        <p style="font-size: 12px;">Chief Diversity, Equity and Inclusion Officer<br><br>
-                                        </p>
+                                        <br>
+                                        <br>
 
                                         <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
                                         <div class="description">
                                             <a href="vpdei/documents/resumes/DJ-CV.pdf" target="_blank">View Resume</a>
-
+                                            <!-- <a style="float: right;" href="https://www.surveymonkey.com/r/MolloyProvostCandidateFeedbackSH" target="_blank">Submit Feedback</a>  -->
                                         </div>
                                     </div>
-
                                 </div>
-
-							<!-- KS (POST AT 5pm am on November 8th)  -->
-                                <!-- <div class="col-sm-12" style="margin: 20px auto;background-color: #fff; border: 1px solid #ccc; border-radius: 2px/4px; box-shadow: 0 0 5px #ccc;height: 200px; padding: 12px 10px;position: relative;">
-                                    <div class="content">
-                                        <p style='font-weight: bold;'>Candidate #3: Tuesday, November 9th, 2021 (10:30 - 11:15am) & (1:00 - 1:45pm)</p>
-                                        <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
-                                        <h4 style="font-weight: bold;">Kahan Sablo</h4>
-                                        <p style="font-size: 12px;">Dean of Inclusive Excellence (Chief Diversity Officer)<br><br>
-                                        </p>
-
-                                        <hr style="border-color: #ccc;margin-top: 5px;margin-bottom: 5px;">
-                                        <div class="description">
-                                            <a href="vpdei/documents/resumes/KS-CV.pdf" target="_blank">View Resume</a>
-                                        </div>
-                                    </div>
-
-                                </div> -->
-
-                                
                             </div>
 							
                             <p><strong>In order to protect the privacy of each of the candidates, we ask that you not disclose the name of the candidate until a new VP is hired.</strong></p>
